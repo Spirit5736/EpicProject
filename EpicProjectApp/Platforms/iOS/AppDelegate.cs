bd91de6b4bf4116143c50d2a1ae09d5badf5e1f4
ivk <ivan.keynes@bbconsult.co.uk>
2023-08-29 22:05:36 +0200

    Add project files.

---
+++ EpicProjectApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EpicProjectApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
